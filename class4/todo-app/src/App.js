a389ba1a66d91f6dd079c6cda3d11eb25c67987d
thanhvdt <79709336+thanhvdt@users.noreply.github.com>
2022-07-25 23:38:35 +0700

    Create App.js

---
+++ class4/todo-app/src/App.js
@@ -0,0 +1,71 @@
+import "./App.css";
+import { useState } from "react";
+import Header from "./components/header";
+import Tasks from "./components/tasks";
+import AddTask from "./components/AddTask";
+import About from "./components/about";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Kaguya-sama",
+      time: "20/07/2022 19:30",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Konosuba",
+      time: "20/07/2022 19:30",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Shingeki no Kyojin",
+      time: "20/07/2022 19:30",
+      reminder: true,
+    },
+    {
+      id: 4,
+      text: "Jujustu Kaisen",
+      time: "20/07/2022 19:30",
+      reminder: true,
+    },
+  ]);
+
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 1000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((remainTask) => remainTask.id !== id));
+  };
+
+  
+
+  return (
+    <BrowserRouter>
+      <div className="container">
+        <Header title="techainer bootcamp" year="2022" />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <AddTask onAdd={addTask} />
+                <Tasks tasks={tasks} onAdd={addTask} />
+                <Link to="/about">About page</Link>
+              </>
+            }
+          ></Route>
+          <Route path="/about" element={<About />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
